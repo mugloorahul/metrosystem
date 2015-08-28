279934697d64d2a794dbeb3a3670f4d3530a6cd7
mugloorahul <mugloorahul@yahoo.com>
2015-08-28 14:02:58 +0530

    Create PaymentException.java

---
+++ MetroSystemClient/Com/Metrosystem/beans/exception/payment/exception/PaymentException.java
@@ -0,0 +1,22 @@
+package com.metrosystem.payment.exception;
+
+public class PaymentException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PaymentException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+	
+	public PaymentException(String msg) {
+		super(msg);
+	}
+
+	public PaymentException(Throwable cause) {
+		super(cause);
+	}
+	
+}
