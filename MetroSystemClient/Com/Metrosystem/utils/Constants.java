29539c00dac409b7091ec0023ebc80184fe4367c
mugloorahul <mugloorahul@yahoo.com>
2015-08-28 14:07:18 +0530

    Create Constants.java

---
+++ MetroSystemClient/Com/Metrosystem/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.metrosystem.utils;
+
+public class Constants {
+
+	public static final double MINIMUM_SMART_CARD_BALANCE = 10;
+	public static final int DEFAULT_NO_OF_TRAIN_SCHEDULER_THREADS = 10;
+	public static final int DEFAULT_NO_OF_USER_SCHEDULER_THREADS = 50;
+	public static final long TRAIN_ARRIVAL_DEPARTURE_DURATION_IN_SECONDS = 60;
+	public static final long NEXT_TRAIN_DEPARTURE_DURATION_IN_SECONDS = 300;
+	public static final String USER_LOG_DIRECTORY="C:\\work\\personal\\metro_system\\logs\\user_logs";
+	public static final String TRAIN_LOG_DIRECTORY="C:\\work\\personal\\metro_system\\logs\\train_logs";
+	
+}
