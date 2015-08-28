b20a380f954370f3dc0f26f38ae0f0f1ab460fc5
mugloorahul <mugloorahul@yahoo.com>
2015-08-28 12:59:36 +0530

    Create BankAccountDaoImpl.java

---
+++ MetroSystemDao/src/main/java/com/metrosystem/dao/impl/BankAccountDaoImpl.java
@@ -0,0 +1,52 @@
+package com.metrosystem.dao.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.metrosystem.dao.IBankAccountDao;
+import com.metrosystem.dao.beans.BankAccountDTO;
+import com.metrosystem.dao.exception.MetroSystemDaoException;
+
+@Repository("accountDao")
+@Transactional(readOnly=true,rollbackFor={Exception.class})
+public class BankAccountDaoImpl extends MetroSystemDaoImpl<Integer, BankAccountDTO> 
+implements IBankAccountDao {
+
+	
+	public BankAccountDaoImpl(){
+		super(BankAccountDTO.class);
+	}
+
+	@Override
+	public List<BankAccountDTO> queryBankAccountsForUser(String userIdentifier) throws MetroSystemDaoException {
+		
+		try{
+			String query = "SELECT account FROM BankAccountDTO account " +
+		                   " WHERE account.user.uniqueIdentifier = ?";
+			
+			return this.queryListOfEntities(query, userIdentifier);
+		}
+		catch(Throwable e){
+			throw new MetroSystemDaoException(e);
+		}
+	}
+
+	@Override
+	public BankAccountDTO queryAccountByNumber(String accountNumber) throws MetroSystemDaoException {
+		
+		try{
+			String query = "FROM BankAccountDTO where accountNumber = ?";
+			List<BankAccountDTO> accounts = this.queryListOfEntities(query, accountNumber);
+			if(accounts == null || accounts.size() == 0){
+				return null;
+			}
+			
+			return accounts.get(0);
+		}
+		catch(Throwable e){
+			throw new MetroSystemDaoException(e);
+		}
+	}
+}
