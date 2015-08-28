782d06f4a3c0ab5a1dab9f8834f897fb1369b80c
mugloorahul <mugloorahul@yahoo.com>
2015-08-28 13:41:25 +0530

    Create IMetroTrainService.java

---
+++ MetroSystemService/src/main/java/com/metrosystem/service/IMetroTrainService.java
@@ -0,0 +1,25 @@
+package com.metrosystem.service;
+
+import java.util.List;
+
+import com.metrosystem.service.beans.MetroTrainBO;
+import com.metrosystem.service.exception.MetroSystemServiceException;
+
+public interface IMetroTrainService {
+
+	public Integer createTrain(int trainNumber,String name, String routeName) throws MetroSystemServiceException;
+	
+	public Integer createTrain(int trainNumber,String name) throws MetroSystemServiceException;
+	
+	public MetroTrainBO findTrainByNumber(int trainNumber) throws MetroSystemServiceException;
+	
+	public MetroTrainBO findTrainByName(String name) throws MetroSystemServiceException;
+	
+	public void assignRouteToTrain(String routeName, int trainNumber) throws MetroSystemServiceException;
+
+	public void assignRouteToTrain(String routeName, String trainName) throws MetroSystemServiceException;
+	
+	public void createMultipleTrains(List<MetroTrainBO> trains) throws MetroSystemServiceException;
+	
+	public List<MetroTrainBO> getTrainsForRoute(String routeName) throws MetroSystemServiceException; 
+}
