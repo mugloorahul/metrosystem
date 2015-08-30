9cc8aebec435ac855837a0875a60c6d561f21077
mugloorahul <mugloorahul@yahoo.com>
2015-08-30 19:29:44 +0530

    Commit

--- scripts/shell/database_client_start.bat
+++ scripts/shell/database_client_start.bat
@@ -1,9 +1,9 @@
-cd C:\work\personal\metro_system\database\derby\databases
+cd G:\work\java\MetroSystem\databases
 
-SET DERBY_INSTALL=C:\work\personal\metro_system\database\derby
+SET DERBY_INSTALL=G:\work\java\MetroSystem\databases\db-derby-10.11.1.1-bin
 
 SET CLASSPATH=%DERBY_INSTALL%\lib\derby.jar;%DERBY_INSTALL%\lib\derbynet.jar;%DERBY_INSTALL%\lib\derbytools.jar;%DERBY_INSTALL%\lib\derbyclient.jar
 
 java org.apache.derby.tools.ij
 
-connect 'jdbc:derby://localhost:1527/MetroSystemDB;user=admin;password=admin';
+connect 'jdbc:derby://localhost:1527/MetroSystemDB;create=true;user=admin;password=admin';
--- scripts/shell/database_server_start.bat
+++ scripts/shell/database_server_start.bat
@@ -1,6 +1,6 @@
-cd C:\work\personal\metro_system\database\derby\databases
+cd G:\work\java\MetroSystem\databases
 
-SET DERBY_INSTALL=C:\work\personal\metro_system\database\derby
+SET DERBY_INSTALL=G:\work\java\MetroSystem\databases\db-derby-10.11.1.1-bin
 
 SET CLASSPATH=%DERBY_INSTALL%\lib\derby.jar;%DERBY_INSTALL%\lib\derbynet.jar;%DERBY_INSTALL%\lib\derbytools.jar;%DERBY_INSTALL%\lib\derbyclient.jar
 
