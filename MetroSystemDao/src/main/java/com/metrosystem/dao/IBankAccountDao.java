841d2fdb18e76c1f50c29be448869caf0b895ef0
mugloorahul <mugloorahul@yahoo.com>
2015-09-01 11:29:48 +0530

    Update IBankAccountDao.java

--- MetroSystemDao/src/main/java/com/metrosystem/dao/IBankAccountDao.java
+++ MetroSystemDao/src/main/java/com/metrosystem/dao/IBankAccountDao.java
@@ -7,8 +7,11 @@ import com.metrosystem.dao.exception.MetroSystemDaoException;
 
 public interface IBankAccountDao extends IMetroSystemDao<Integer, BankAccountDTO> {
 
+	public List<BankAccountDTO> queryActiveBankAccountsForUser(String userIdentifier) throws MetroSystemDaoException;
+
+	public BankAccountDTO queryActiveBankAccountByNumber(String accountNumber) throws MetroSystemDaoException;
+  
 	public List<BankAccountDTO> queryBankAccountsForUser(String userIdentifier) throws MetroSystemDaoException;
 
-	public BankAccountDTO queryAccountByNumber(String accountNumber) throws MetroSystemDaoException;
-  
+	public BankAccountDTO queryBankAccountByNumber(String accountNumber) throws MetroSystemDaoException;
 }
