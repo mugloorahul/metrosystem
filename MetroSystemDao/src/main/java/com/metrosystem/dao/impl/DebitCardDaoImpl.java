e1a36d8585b24ba114e5afd047f37fec8c04e939
mugloorahul <mugloorahul@yahoo.com>
2015-09-01 11:49:23 +0530

    Update DebitCardDaoImpl.java

--- MetroSystemDao/src/main/java/com/metrosystem/dao/impl/DebitCardDaoImpl.java
+++ MetroSystemDao/src/main/java/com/metrosystem/dao/impl/DebitCardDaoImpl.java
@@ -20,7 +20,8 @@ public class DebitCardDaoImpl extends MetroSystemDaoImpl<Integer, DebitCardDTO> 
 		super(DebitCardDTO.class);
 	}
 	
-    public DebitCardDTO queryCardByNumber(String cardNumber) throws MetroSystemDaoException{
+	@Override
+    public DebitCardDTO queryActiveCardByNumber(String cardNumber) throws MetroSystemDaoException{
     	
     	try{
     		String query = "FROM DebitCardDTO WHERE paymentId =? AND deleted != ?";
@@ -37,7 +38,8 @@ public class DebitCardDaoImpl extends MetroSystemDaoImpl<Integer, DebitCardDTO> 
     	}
     }
 	
-	public List<DebitCardDTO> queryCardsByUser(String userIdentifier) throws MetroSystemDaoException{
+	@Override
+	public List<DebitCardDTO> queryActiveCardsByUser(String userIdentifier) throws MetroSystemDaoException{
 		
 		try{
 			String query = "SELECT pay,account " +
@@ -65,7 +67,8 @@ public class DebitCardDaoImpl extends MetroSystemDaoImpl<Integer, DebitCardDTO> 
     	}
 	}
 	
-	public List<DebitCardDTO> queryCardByAccount(String accountNumber) throws MetroSystemDaoException{
+	@Override
+	public List<DebitCardDTO> queryActiveCardsByAccount(String accountNumber) throws MetroSystemDaoException{
 		
 		try{
 			String query = "FROM DebitCardDTO pay" +
@@ -79,4 +82,62 @@ public class DebitCardDaoImpl extends MetroSystemDaoImpl<Integer, DebitCardDTO> 
     	}
 	}
 	
+	@Override
+    public DebitCardDTO queryCardByNumber(String cardNumber) throws MetroSystemDaoException{
+    	
+    	try{
+    		String query = "FROM DebitCardDTO WHERE paymentId =?";
+    		
+    		List<DebitCardDTO> cards = this.queryListOfEntities(query, cardNumber);
+    		if(cards == null || cards.size() == 0){
+    			return null;
+    		}
+    		
+    		return cards.get(0);
+    	}
+    	catch(Throwable e){
+    		throw new MetroSystemDaoException(e);
+    	}
+    }
+	
+	@Override
+	public List<DebitCardDTO> queryCardsByUser(String userIdentifier) throws MetroSystemDaoException{
+		
+		try{
+			String query = "SELECT pay,account " +
+		                   " FROM DebitCardDTO pay" +
+					       " INNER JOIN pay.account account"+
+		                   " INNER JOIN account.user user " +
+					       " WHERE user.uniqueIdentifier = ?";
+			
+			 List<?> result= this.queryListOfEntities(query, userIdentifier);
+			 
+			 List<DebitCardDTO> cards = new ArrayList<DebitCardDTO>();
+			 for(int i =0; i < result.size(); i++){
+				 Object[] entities = (Object[])result.get(i);
+				 DebitCardDTO card = (DebitCardDTO) entities[0];
+				 card.setAccount((BankAccountDTO)entities[1]);
+				 cards.add(card);
+			 }
+			 
+			 return cards;
+		}
+		catch(Throwable e){
+    		throw new MetroSystemDaoException(e);
+    	}
+	}
+	
+	@Override
+	public List<DebitCardDTO> queryCardsByAccount(String accountNumber) throws MetroSystemDaoException{
+		
+		try{
+			String query = "FROM DebitCardDTO pay" +
+	                       " WHERE pay.account.accountNumber= ?";
+		
+		    return this.queryListOfEntities(query, accountNumber);
+		}
+		catch(Throwable e){
+    		throw new MetroSystemDaoException(e);
+    	}
+	}
 }
