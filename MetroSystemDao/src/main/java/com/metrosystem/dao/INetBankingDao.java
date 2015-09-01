04d14bb1d973536160f0055cc5329e9fd6bf5eba
mugloorahul <mugloorahul@yahoo.com>
2015-09-01 11:48:02 +0530

    Update INetBankingDao.java

--- MetroSystemDao/src/main/java/com/metrosystem/dao/INetBankingDao.java
+++ MetroSystemDao/src/main/java/com/metrosystem/dao/INetBankingDao.java
@@ -7,7 +7,13 @@ import com.metrosystem.dao.exception.MetroSystemDaoException;
 
 public interface INetBankingDao extends IMetroSystemDao<Integer, NetBankingDTO> {
 
-	public NetBankingDTO queryByCustomerId(String customerId) throws MetroSystemDaoException;
+	public NetBankingDTO queryActiveByCustomerId(String customerId) throws MetroSystemDaoException;
+	
+	public List<NetBankingDTO> queryActiveByUser(String userIdentifier) throws MetroSystemDaoException;
+	
+	public NetBankingDTO queryActiveByAccountNumber(String accountNumber) throws MetroSystemDaoException;
+	
+    public NetBankingDTO queryByCustomerId(String customerId) throws MetroSystemDaoException;
 	
 	public List<NetBankingDTO> queryByUser(String userIdentifier) throws MetroSystemDaoException;
 	
